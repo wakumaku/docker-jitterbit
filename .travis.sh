f4fcb7a0076ba0120bf15423e6de3aead7586c1b
jordi <jordi.tarruella@bandit.io>
2017-02-22 14:43:12 +0100

    Adds travis-ci.org scripts

---
+++ .travis.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -ev
+
+MAX_ITERATIONS=6
+TIMEOUT_ITERARION=5
+
+for i in {1..${MAX_ITERATIONS}}; do
+    STATUS=`docker exec -t gallant_jepsen jitterbit status`
+    echo "Checking service status ..."
+    echo "${STATUS}"
+    ASSERT=`echo "${STATUS}" | grep "All services are running" | wc -l`
+    if [ "${ASSERT}" == 1 ]; then
+        exit 0
+    fi
+    sleep ${TIMEOUT_ITERARION}
+done
+
+exit 1
