edeac052eb3946fbf5abbabf10077941d9ef3a55
jordi <jordi.tarruella@bandit.io>
2017-03-04 23:12:41 +0100

    fix startup script

--- scripts/startup/00_startup.sh
+++ scripts/startup/00_startup.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
 if [ ! -f ~/installation_done ]; then
+    echo "### Installing AGENT"
     dpkg -i ${INSTALL_DIR}/${INSTALL_FILE}
+    echo "### Stopping AGENT"    
+    jitterbit stop
+    echo "### Configuring AGENT"    
     jitterbit-config \
         --user="${JB_USERNAME}" \
         --password="${JB_PASSWORD}" \
         --organization="${JB_ORGANIZATION}" \
         --agent-group="${JB_AGENT_GROUP}" \
         --agent="${JB_AGENT}"
+    echo "### Initializing Database"        
     jitterbit initdb
     touch ~/installation_done
     rm ${JITTERBIT_HOME}/*.gz
     rm -rf ${INSTALL_DIR}
 fi
 
-jitterbit restart
+echo "### Starting AGENT"    
+jitterbit start
